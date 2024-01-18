d4a30923646c5e703d8b220c5cef479062758d49
ramya-2701 <95271051+ramya-2701@users.noreply.github.com>
2024-01-18 13:23:15 -0600

    Reverse an array

---
+++ arrays/ReversingArray.java
@@ -0,0 +1,8 @@
+public class ReversingArray {
+    public static void main(String[] args){
+        int arr[]={3,5,6,4,7};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]);
+        }
+    }
+}
